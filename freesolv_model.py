e925b3e4436cfa4bdcffcda1776acd94412ae3dc
Olivia Alcabes <oliviaalcabes@Olivias-MacBook-Pro.local>
2021-03-19 20:04:12 -0500

    uncommented kfold

--- freesolv_model.py
+++ freesolv_model.py
@@ -200,93 +200,93 @@ trainer.train(device=device, n_epochs=n_epochs)
 #repeat_results = pd.DataFrame(data=repeat_results)
 #repeat_results.to_csv('freesolv_kfold_results.csv')
 #FREESOLV VERSION OF KFOLD
-#kfold_results= {}
-#kfold_results['learning_rates']=[]
-#kfold_results['train_losses']=[]
-#kfold_results['val_losses']=[]
-#kfold_results['val_maes']=[]
+kfold_results= {}
+kfold_results['learning_rates']=[]
+kfold_results['train_losses']=[]
+kfold_results['val_losses']=[]
+kfold_results['val_maes']=[]
 
 #def mse_loss(batch, result):
 #    diff = batch['expt']-result['expt']
 #    err_sq = torch.mean(diff ** 2)
 #    return err_sq
 
-#k = 5
+k = 5
 
-#kfold = KFold(n_splits=k,shuffle=True)
+kfold = KFold(n_splits=k,shuffle=True)
     
-#for fold, (train_ids, val_ids) in enumerate(kfold.split(new_dataset)):
+for fold, (train_ids, val_ids) in enumerate(kfold.split(new_dataset)):
 #print('train: %s, test: %s' % (data[train], data[test]))
     
-#    print('train: %s, test: %s' %(train_ids,val_ids))
+    print('train: %s, test: %s' %(train_ids,val_ids))
         
-#    train_data = new_dataset.create_subset(train_ids)
-#    val_data = new_dataset.create_subset(val_ids)
+    train_data = new_dataset.create_subset(train_ids)
+    val_data = new_dataset.create_subset(val_ids)
     
-
-
 
 #NOTE --- NEED TO CHANGE THIS FROM QM9
 
 # build optimizer
 
-#loss = trn.build_mse_loss(['expt'])
+#
     
 #    print('train data: %s, test data: %s' %(train_data,val_data))
     
-#    train_loader = spk.AtomsLoader(train_data, batch_size=50, shuffle=True)
-#    val_loader = spk.AtomsLoader(val_data, batch_size=50)
+    train_loader = spk.AtomsLoader(train_data, batch_size=50, shuffle=True)
+    val_loader = spk.AtomsLoader(val_data, batch_size=50)
     
-#    schnet = spk.representation.SchNet(
-#        n_atom_basis=30, n_filters=30, n_gaussians=20, n_interactions=5,
-#        cutoff=4., cutoff_network=spk.nn.cutoff.CosineCutoff
-#    )
+    schnet = spk.representation.SchNet(
+        n_atom_basis=30, n_filters=30, n_gaussians=20, n_interactions=5,
+        cutoff=4., cutoff_network=spk.nn.cutoff.CosineCutoff
+    )
 
-#    output = spk.atomistic.Atomwise(n_in=30, property='expt')
+    output = spk.atomistic.Atomwise(n_in=30, property='expt')
     
-#    model = spk.AtomisticModel(representation=schnet, output_modules=output)
+    model = spk.AtomisticModel(representation=schnet, output_modules=output)
     
 
     # build optimizer
-#    optimizer = Adam(model.parameters(), lr=1e-2)
+    optimizer = Adam(model.parameters(), lr=1e-2)
 
 #    loss = mse_loss
 
-#    metrics = [spk.metrics.MeanAbsoluteError('expt')]
-#    hooks = [
-#        trn.CSVHook(log_path=freesolvmod, metrics=metrics),
-#        trn.ReduceLROnPlateauHook(
-#            optimizer,
-#            patience=5, factor=0.8, min_lr=1e-6,
-#            stop_after_min=True
-#        )
-#    ]
+    loss = trn.build_mse_loss(['expt'])
 
-#    trainer = trn.Trainer(
-#        model_path=freesolvmod,
-#        model=model,
-#        hooks=hooks,
-#        loss_fn=loss,
-#        optimizer=optimizer,
-#        train_loader=train_loader,
-#        validation_loader=val_loader,
-#    )
+    metrics = [spk.metrics.MeanAbsoluteError('expt')]
+    hooks = [
+        trn.CSVHook(log_path=freesolvmod, metrics=metrics),
+        trn.ReduceLROnPlateauHook(
+            optimizer,
+            patience=5, factor=0.8, min_lr=1e-6,
+            stop_after_min=True
+        )
+    ]
+
+    trainer = trn.Trainer(
+        model_path=freesolvmod,
+        model=model,
+        hooks=hooks,
+        loss_fn=loss,
+        optimizer=optimizer,
+        train_loader=train_loader,
+        validation_loader=val_loader,
+    )
 #    
-#    device = "cpu" # change to 'cpu' if gpu is not available, change to cuda if gpu is
-#    n_epochs = 25 # takes about 10 min on a notebook GPU. reduces for playing around
-#    trainer.train(device=device, n_epochs=n_epochs)
+    device = "cpu" # change to 'cpu' if gpu is not available, change to cuda if gpu is
+    n_epochs = 25 # takes about 10 min on a notebook GPU. reduces for playing around
+    trainer.train(device=device, n_epochs=n_epochs)
 #    
-#    results = np.loadtxt(os.path.join(freesolvmod, 'log.csv'), skiprows=1, delimiter=',')
+    results = np.loadtxt(os.path.join(freesolvmod, 'log.csv'), skiprows=1, delimiter=',')
+    # 
+    learning_rate = results[:,1]
+    train_loss = results[:,2]
+    val_loss = results[:,3]
+    val_mae = results[:,4]
     
-#    learning_rate = results[:,1]
-#    train_loss = results[:,2]
-#    val_loss = results[:,3]
-#    val_mae = results[:,4]
-    
-#    kfold_results['learning_rates'].append(learning_rate)
-#    kfold_results['train_losses'].append(train_loss)
-#    kfold_results['val_losses'].append(val_loss)
-#    kfold_results['val_maes'].append(val_mae)
+    kfold_results['learning_rates'].append(learning_rate)
+    kfold_results['train_losses'].append(train_loss)
+    kfold_results['val_losses'].append(val_loss)
+    kfold_results['val_maes'].append(val_mae)
 #    
-#kfold_results = pd.Dataframe(data=kfold_results)
-#kfold_results.to_csv('freesolv_kfold_results.csv')
+kfold_results = pd.Dataframe(data=kfold_results)
+kfold_results.to_csv('freesolv_kfold_results.csv')
